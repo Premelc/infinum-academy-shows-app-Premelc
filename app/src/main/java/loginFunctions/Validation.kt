98be3478d6147258e75a0038d1e04ee8fb91ccf3
premelc <dominik.premelc33@gmail.com>
2022-07-12 15:26:52 +0200

    simplify validation functions

--- app/src/main/java/loginFunctions/Validation.kt
+++ app/src/main/java/loginFunctions/Validation.kt
@@ -1,15 +1,11 @@
 package loginFunctions
 
     fun validateEmail(email: String) : Boolean {
-        if (email.isEmpty()) return false
-        else {
-            return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
-        }
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
     fun validatePassword(password: String) : Boolean{
-        if (password.length < 6)return false;
-        return true;
+        return password.length >= 6
     }
 
 fun validateLoginData(email:String,password:String): Boolean{
