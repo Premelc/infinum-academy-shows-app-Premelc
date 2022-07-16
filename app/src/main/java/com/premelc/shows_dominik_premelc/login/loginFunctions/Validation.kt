f51507ad4067422a448be22a5c63aa1a2e60937b
premelc <dominik.premelc33@gmail.com>
2022-07-16 21:54:05 +0200

    fix hardcoded Int in validation file

--- app/src/main/java/com/premelc/shows_dominik_premelc/login/loginFunctions/Validation.kt
+++ app/src/main/java/com/premelc/shows_dominik_premelc/login/loginFunctions/Validation.kt
@@ -1,11 +1,13 @@
 package com.premelc.shows_dominik_premelc.login.loginFunctions
 
+private const val PASSWORD_MIN_LENGTH = 6
+
 fun validateEmail(email: String): Boolean {
     return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 }
 
 fun validatePassword(password: String): Boolean {
-    return password.length >= 6
+    return password.length >= PASSWORD_MIN_LENGTH
 }
 
 fun validateLoginData(email: String, password: String): Boolean {
